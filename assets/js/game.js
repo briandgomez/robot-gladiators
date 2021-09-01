8161e071659b2f880fa04b3799885a3e720166fe
briandgomez <briandgomez98@gmail.com>
2021-08-31 22:56:58 -0400

    shop-input enhancement

--- assets/js/game.js
+++ assets/js/game.js
@@ -151,20 +151,18 @@ var endGame = function () {
 
 var shop = function () {
     //ask player what they'd like to do
-    var shopOptionPrompt = window.prompt("Would youlike to REFILL your health, UPGRADE your attack, or LEAVE the store? Please eneter one: 'REFILL','UPGRADE',or 'LEAVE' to make choice.");
+    var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE ,or 3 for  LEAVE.");
 
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     //use switch to carr out action
     switch (shopOptionPrompt) {
-        case "REFILL": //new case
-        case "refill":
+        case 1: //new case
             playerInfo.refillHealth();
             break;
-        case "UPGRADE": //new case
-        case "upgrade":
+        case 2: //new case
             playerInfo.upgradeAttack();
             break;
-        case "LEAVE"://new case
-        case "leave":
+        case 3://new case
             window.alert("Leaving store.")
             //do nothing so function will end
             break;
